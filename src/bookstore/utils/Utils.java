8774ec87ec62ed79946ded9884677f96c63ea0dd
HaiLeVN <37034176+HaiLeVN@users.noreply.github.com>
2023-02-13 12:11:42 +0700

    created templates

---
+++ src/bookstore/utils/Utils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.utils;
+
+/**
+ *
+ * @author Thanh Hai
+ */
+public class Utils {
+    
+}
